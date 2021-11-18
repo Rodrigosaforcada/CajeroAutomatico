74eedd34b87881db113d7dcb4ec3b98983bb0363
RavarottoIvan <ivanravarotto@gmail.com>
2021-11-18 17:23:29 -0300

    primer cambio

--- src/Main/MainClass.java
+++ src/Main/MainClass.java
@@ -1,8 +1,12 @@
 package Main;
+import javax.sound.sampled.SourceDataLine;
+
 import Funciones.*;
 
 public class MainClass {
     public static void main(String[] args) {
+
+        System.out.println("Hola mundo");
 
         Opciones mensajero_opciones = new Consulta();
         mensajero_opciones.setSaldo(6500);
